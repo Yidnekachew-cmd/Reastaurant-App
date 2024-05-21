a0b441c91dacada881381db3136509e12815fabc
Yidne <Yamiyekn@gmail.com>
2024-05-21 19:47:18 +0300

    Render the components in the App component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React from "react";
+import {
+	AboutUs,
+	Chef,
+	FindUs,
+	Footer,
+	Gallery,
+	Header,
+	Intro,
+	Laurels,
+	SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<h1>My first React App</h1>
-		</div>
-	);
-}
+const App = () => (
+	<div>
+		<Navbar />
+		<Header />
+		<AboutUs />
+		<SpecialMenu />
+		<Chef />
+		<Intro />
+		<Laurels />
+		<Gallery />
+		<FindUs />
+		<Footer />
+	</div>
+);
 
 export default App;
